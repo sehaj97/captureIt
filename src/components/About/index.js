319f0035704c5433ce3d576d598f1c00e866017b
sehaj97 <sehajmagan@gmail.com>
2022-02-27 17:33:38 -0500

    completed bnav componnents

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,7 +3,7 @@ import coverImage from "../../assets/cover/cover-image.jpg";
 
 function About() {
   return (
-    <section className="my-5">
+    <section className="my-3">
         <h1 id="about">Who am I?</h1>
         <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
     </section>
